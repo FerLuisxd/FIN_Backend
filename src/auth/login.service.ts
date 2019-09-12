0733a8478df12d6a9e29d0ca060a6919d36b3fce
Luis Vilca <ferluisxd@gmail.com>
2019-09-12 17:31:26 -0500

    a

--- src/auth/login.service.ts
+++ src/auth/login.service.ts
@@ -5,34 +5,29 @@ const DB = require('better-sqlite3-helper');
 
 @Injectable()
 export class LoginService {
-
-  constructor(
-    private authHelper: AuthHelper
-  ) { }
+  constructor(private authHelper: AuthHelper) {}
 
   /*******************************************************
    * Basic Login with credentials
    *******************************************************/
   public login(email, password) {
-
     if (!email) {
-      return (new HttpException("Email is required", 422))
+      return new HttpException('Email is required', 422);
     }
     if (!password) {
-      return (new HttpException("Password is required", 422))
+      return new HttpException('Password is required', 422);
     }
 
     return new Promise((resolve, reject) => {
       let row = DB().queryFirstRow('SELECT * FROM user WHERE email=?', email);
-      if(row){
-      if(row.password != password){
-        return reject(new HttpException("Incorrect password", 401))
+      if (row) {
+        if (row.password != password) {
+          return reject(new HttpException('Incorrect password', 401));
+        } else {
+          return resolve(this.authHelper.genToken(row));
+        }
       }
-      else{
-        return resolve(this.authHelper.genToken(row))
-      }
-    }
-
+      //
       // db.all(
       //   "SELECT * FROM user WHERE email = ?", [email], (err, user) => {
 
@@ -56,5 +51,4 @@ export class LoginService {
       //   });
     });
   }
-
 }
